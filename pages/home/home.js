5b07aedca553a48089b447812704fd643be54007
unknown <unknown@example.com>
2023-04-02 17:57:44 +0800

    首页的书写

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,20 +1,63 @@
-import { fetchHome } from '../../services/home/home';
-import { fetchGoodsList } from '../../services/good/fetchGoods';
+import {
+  fetchHome
+} from '../../services/home/home';
+import {
+  fetchGoodsList
+} from '../../services/good/fetchGoods';
 import Toast from 'tdesign-miniprogram/toast/index';
 
 Page({
   data: {
     imgSrcs: [],
-    tabList: [],
-    goodsList: [],
+    tabList: [{
+      text: "推荐",
+      key: 0
+    }, {
+      text: "学校",
+      key: 1
+    }, {
+      text: "专业",
+      key: 2
+    }, {
+      text: "长期",
+      key: 3
+    }, {
+      text: "短期",
+      key: 4
+    }, {
+      text: "线上",
+      key: 5
+    }, {
+      text: "线下",
+      key: 6
+    }],
+    internlist: [{
+      id: 1,
+      title: "计算机视觉实习",
+      content: "传统目标检测，无人驾驶",
+      tags: ["3个月", "线下", "CV", "python", "3人"],
+      tname: "刘偲教授",
+      school: "北京航空航天大学"
+    }, {
+      id: 2,
+      title: "计算机视觉实习",
+      content: "对于diffusion model infer阶段算法加速",
+      tags: ["5个月", "线上", "CV", "python", "1人"],
+      tname: "朱军教授",
+      school: "清华大学"
+    }],
     goodsListLoadStatus: 0,
     pageLoading: false,
     current: 1,
     autoplay: true,
     duration: '500',
     interval: 5000,
-    navigation: { type: 'dots' },
-    swiperImageProps: { mode: 'scaleToFill' },
+    navigation: {
+      type: 'dots'
+    },
+    swiperImageProps: {
+      mode: 'scaleToFill'
+    },
   },
 
   goodListPagination: {
@@ -35,9 +78,7 @@ Page({
   },
 
   onReachBottom() {
-    if (this.data.goodsListLoadStatus === 0) {
-      this.loadGoodsList();
-    }
+    if (this.data.goodsListLoadStatus === 0) {}
   },
 
   onPullDownRefresh() {
@@ -51,79 +92,54 @@ Page({
   loadHomePage() {
     wx.stopPullDownRefresh();
 
+    // this.setData({
+    //   pageLoading: true,
+    // });
+
+    // fetchHome().then(({
+    //   swiper,
+    //   tabList
+    // }) => {
+    //   this.setData({
+    //     tabList,
+    //     imgSrcs: swiper,
+    //     pageLoading: false,
+    //   });
+    //   this.loadGoodsList(true);
+    // });
+    wx.request({
+      url: getApp().globalData.baseUrl + '/getRecommend',
+      header: {
+        Authorization: wx.getStorageSync('token'),
+      },
+      method: 'POST',
+      success: (res) => {
+        this.setData({
+          internlist: res.data.internlist
+        })
+      }
+    })
     this.setData({
-      pageLoading: true,
-    });
-    fetchHome().then(({ swiper, tabList }) => {
-      this.setData({
-        tabList,
-        imgSrcs: swiper,
-        pageLoading: false,
-      });
-      this.loadGoodsList(true);
+      pageLoading: false,
     });
   },
 
   tabChangeHandle(e) {
     this.privateData.tabIndex = e.detail;
-    this.loadGoodsList(true);
-  },
-
-  onReTry() {
-    this.loadGoodsList();
-  },
-
-  async loadGoodsList(fresh = false) {
-    if (fresh) {
-      wx.pageScrollTo({
-        scrollTop: 0,
-      });
-    }
-
-    this.setData({ goodsListLoadStatus: 1 });
-
-    const pageSize = this.goodListPagination.num;
-    let pageIndex = this.privateData.tabIndex * pageSize + this.goodListPagination.index + 1;
-    if (fresh) {
-      pageIndex = 0;
-    }
-
-    try {
-      const nextList = await fetchGoodsList(pageIndex, pageSize);
-      this.setData({
-        goodsList: fresh ? nextList : this.data.goodsList.concat(nextList),
-        goodsListLoadStatus: 0,
-      });
-
-      this.goodListPagination.index = pageIndex;
-      this.goodListPagination.num = pageSize;
-    } catch (err) {
-      this.setData({ goodsListLoadStatus: 3 });
-    }
-  },
-
-  goodListClickHandle(e) {
-    const { index } = e.detail;
-    const { spuId } = this.data.goodsList[index];
-    wx.navigateTo({
-      url: `/pages/goods/details/index?spuId=${spuId}`,
-    });
-  },
-
-  goodListAddCartHandle() {
-    Toast({
-      context: this,
-      selector: '#t-toast',
-      message: '点击加入购物车',
-    });
   },
 
   navToSearchPage() {
-    wx.navigateTo({ url: '/pages/goods/search/index' });
+    wx.navigateTo({
+      url: '/pages/goods/search/index'
+    });
   },
 
-  navToActivityDetail({ detail }) {
-    const { index: promotionID = 0 } = detail || {};
+  navToActivityDetail({
+    detail
+  }) {
+    const {
+      index: promotionID = 0
+    } = detail || {};
     wx.navigateTo({
       url: `/pages/promotion-detail/index?promotion_id=${promotionID}`,
     });
