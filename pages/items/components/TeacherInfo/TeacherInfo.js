b99243b0218feee2bc7bab231114fa6d3da15a9e
李逸卓 <20373493@buaa.edu.cn>
2023-04-06 02:43:41 +0800

    Just noodling around!!!

---
+++ pages/items/components/TeacherInfo/TeacherInfo.js
@@ -0,0 +1,38 @@
+// pages/items/components/TeacherInfo/TeacherInfo.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    avatarUrl: {
+      type: String,
+      value: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg"
+    },
+    name: {
+      type: String,
+      value: "刘偲"
+    },
+    title: {
+      type: String,
+      value: "教授"
+    },
+    intro: {
+      type: String,
+      value: "刘偲，北航教授，博导。曾主持国家优秀青年科学基金。博士毕业于中科院自动化所，曾于新加坡国立大学任研究助理和博后，曾任微软亚洲研究院(MSRA)铸星计划研究员。研究方向是跨模态多媒体智能分析（跨模态包含自然语言，计算机视觉以及语音等）以及经典计算机视觉任务（目标检测、跟踪和分割）。个人主页：http://colalab.org/ 。"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    avatarUrl: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/items/details/index.js
+++ pages/items/details/index.js
@@ -5,14 +5,163 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    title: "计算机视觉实习",
+    sub: "刘偲教授",
+    alreadynumber: 2,
+    number: 3,
+    topic: "计算机视觉",
+    location: "北京航空航天大学",
+    // item_info: 365
+    imgSrcs: [],
+    tabList: [{
+      text: "推荐",
+      key: 0
+    }, {
+      text: "学校",
+      key: 1
+    }, {
+      text: "专业",
+      key: 2
+    }, {
+      text: "长期",
+      key: 3
+    }, {
+      text: "短期",
+      key: 4
+    }, {
+      text: "线上",
+      key: 5
+    }, {
+      text: "线下",
+      key: 6
+    }],
+    internlist: [
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }, {
+        id: 2,
+        title: "计算机视觉实习",
+        content: "对于diffusion model infer阶段算法加速",
+        num: 1,
+        alreadynum: 0,
+        tags: ["5个月", "线上", "CV", "python"],
+        tname: "朱军教授",
+        school: "清华大学"
+      }],
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }],
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }],
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }],
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }],
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }],
+      [{
+        id: 1,
+        title: "计算机视觉实习",
+        content: "传统目标检测，无人驾驶",
+        num: 3,
+        alreadynum: 1,
+        tags: ["3个月", "线下", "CV", "python"],
+        tname: "刘偲教授",
+        school: "北京航空航天大学"
+      }],
+    ],
+    goodsListLoadStatus: 0,
+    pageLoading: false,
+    current: 0,
+    autoplay: true,
+    duration: '500',
+    interval: 5000,
+    navigation: {
+      type: 'dots'
+    },
+    swiperImageProps: {
+      mode: 'scaleToFill'
+    },
+    value: 'label_1',
+    list: [{
+        value: 'label_1',
+        label: '首页',
+        icon: 'home'
+      },
+      {
+        value: 'label_2',
+        label: '收藏',
+        icon: 'star'
+      },
+      {
+        value: 'label_3',
+        label: '立即沟通',
+        icon: 'chat'
+      },
+    ],
   },
-
+  methods: {
+    onChange(e) {
+      this.setData({
+        value: e.detail.value,
+      });
+    },
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // console.log(item_info);
+    // // 获取项目id
+    // const item_id = options.item_id
+    // // 调用请求商品详情数据的方法
+    // this.getItemDetail(item_id);
   },
 
   /**
