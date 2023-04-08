a987c28a36207fe292ed6545d56d51dac2e82397
unknown <unknown@example.com>
2023-04-08 17:14:29 +0800

    实现card的click函数，携带id信息跳转到项目详情

--- components/intern-card/index.js
+++ components/intern-card/index.js
@@ -21,8 +21,14 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    click() {
+    click(e) {
       // 组件的点击事件逻辑代码
+      // console.log(e.currentTarget.dataset.item)
+      let id = e.currentTarget.dataset.item.id
+      console.log(id)
+      // wx.navigateTo({
+      //   url: '/pages/destination/destination?id=' + JSON.stringify(id)
+      // })
     }
   }
 })
