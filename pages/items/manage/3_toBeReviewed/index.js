0215a9f618dd3eec61f265252769fc22fcfd0066
李逸卓 <tyler_swift@163.com>
2023-04-24 12:33:52 +0800

    0424-12:33-manage-toBeReviewed与后端成功连接

--- pages/items/manage/3_toBeReviewed/index.js
+++ pages/items/manage/3_toBeReviewed/index.js
@@ -1,4 +1,6 @@
 // pages/items/manage/1_recruiting/index.js
+import Toast from 'tdesign-miniprogram/toast/index';
+
 Page({
   /**
    * 页面的初始数据
@@ -63,44 +65,81 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    // TODO: component中测试
+    // 1. 加载iID
     console.log("in onLoad() function");
     this.data.iId = options.iId;
     console.log("iId=", this.data.iId);
+    // TODO TODO：记得删掉下面这行！！！
+    this.data.iId = '32';
+    // 2. 加载项目相关信息 [iId => iInfos]
+    wx.request({
+      url: getApp().globalData.baseUrl + '/getItemShortInfo',
+      method: 'post',
+      data: {
+        iId: this.data.iId
+      },
+      success: (res) => {
+        if (res.data.success) {
+          console.log("success!!!");
+          console.log(res);
+          this.setData({
+            iTitle: res.data.iTitle,
+            iNum: res.data.iNum,
+            iCapacity: res.data.iCapacity,
+            iDuration: res.data.iDuration,
+            iTime: res.data.iTime,
+            iType: res.data.iType,
+            iInfo: res.data.iInfo
+          });
+        } else {
+          Toast({
+            context: this,
+            selector: '#t-toast',
+            message: "信息加载失败",
+            duration: 1500,
+            theme: 'error',
+            direction: 'column',
+          });
+          wx.navigateBack();
+        }
+      }
+    });
+    // 3. 加载两个学生数组
+    wx.request({
+      url: getApp().globalData.baseUrl + '/iId2alreadyANDyet',
+      data: {
+        iId: this.data.iId
+      },
+      method: 'post',
+      success: (res) => {
+        if (res.data.success) {
+          this.setData({
+            students_yet: res.data.yet,
+            students_already: res.data.already
+          });
+        } else {
+          Toast({
+            context: this,
+            selector: '#t-toast',
+            message: "加载评价学生数据失败",
+            duration: 1500,
+            theme: 'error',
+            direction: 'column',
+          });
+          setTimeout(() => {
+            wx.navigateBack();
+          }, 1500);
+        }
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
+  comment(e) {
+    const index = e.currentTarget.dataset.index;
+    console.log(index);
+    wx.navigateTo({
+      url: '/pages/items/review/mentorReview/index?sId=' + this.data.students_yet[index].sId + '&iId=' + this.data.iId
+    });
+  }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
 });
